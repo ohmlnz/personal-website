47bc3ec36e484da7543d578ed4aeb9719bda5969
OthmanLan <othman.lanizi@gmail.com>
2018-01-26 00:44:17 -0500

    Fix responsiveness.

--- src/Details.js
+++ src/Details.js
@@ -10,18 +10,22 @@ class Details extends Component {
         <h1>Details</h1>
         <h4>(Links on images)</h4>
 
-        <section class='portfolio-full'>
-          <div class='wrapper'>
+        <section className='portfolio-full'>
+          <div className='wrapper'>
             {details.map((d, i) => (
-              <figure>
-                <h3>{d.title}</h3>
-                <a href={d.link} target="_blank" rel="noopener noreferrer">
-                  <img src={d.image} alt={d.title}/>
-                </a>
-                <figcaption>{d.description}
-                  <p><b>Stack:</b> {d.stack}</p>
-                </figcaption>
-              </figure>
+              <div className='sub-wrapper' key={i}>
+                {d.map((p, i) => (
+                  <figure key={i}>
+                    <h3>{p.title}</h3>
+                    <a href={p.link} target="_blank" rel="noopener noreferrer">
+                      <img src={p.image} alt={p.title}/>
+                    </a>
+                    <figcaption>{p.description}
+                      <p><b>Stack:</b> {p.stack}</p>
+                    </figcaption>
+                  </figure>
+                ))}
+              </div>
             ))}
           </div>
         </section>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 
 import Homepage from './Homepage';
 import Details from './Details';
 import NotFound from './NotFound';
 
+const anchors = ['/', '/#intro', '/#about', '/#portfolio', '/#skills', '/#contact'];
+
 const App = () => (
   <Router>
-    <div>
-      <Route exact path="/" component={Homepage}/>
+    <Switch>
+      {anchors.map((path, i) => (
+        <Route exact path={path} component={Homepage} key={i}/>
+      ))}
       <Route exact path="/details" component={Details}/>
       <Route exact path="*" component={NotFound}/>
-    </div>
+    </Switch>
   </Router>
 )
 
